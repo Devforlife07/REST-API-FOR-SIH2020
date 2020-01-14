9f8d764e3bb1a8001e972fceb0e69b521c3701d4
Devforlife07 <Devansh.forlife@gmail.com>
2020-01-14 09:50:39 +0530

    Getting Things Ready

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const app = express();
+dotenv.config({
+  path: "config/config.env"
+});
 app.use(express.json());
 app.get("/", (req, res) => {
   res.send(JSON.stringify(req.headers, null, 2) + "API IS UNDER CONSTRUCTION");
 });
-app.listen(5500, () => {
-  console.log("APP IS  RUUNING");
+const PORT = process.env.PORT || 5500;
+app.listen(PORT, () => {
+  console.log("APP IS  RUUNING ON PORT" + PORT);
 });
