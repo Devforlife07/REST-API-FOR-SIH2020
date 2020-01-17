a293cab3cf533776959c5b26970538ea200be1cc
Devforlife07 <Devansh.forlife@gmail.com>
2020-01-17 17:52:01 +0530

    Unnecessary Routes Deleted

--- routes/login.js
+++ routes/login.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const mongooose = require("mongoose");
 const user = require("../model/user");
 const jwt = require("jsonwebtoken");
+const problem = require("../model/random");
 router.post("/", (req, res) => {
   const {
     uid,
@@ -19,31 +20,22 @@ router.post("/", (req, res) => {
         process.env.SECRET
       );
       res.setHeader("auth", token);
-      res.send({
-        item,
-        token
-      });
+      console.log(item)
+      problem.find({
+        person: item._id
+      }).then(problems => {
+        let problem = [];
+        problems.forEach(x => problem.push(x.problem));
+        console.log(problem);
+        res.send({
+          item,
+          token,
+          problem
+        })
+      })
+
     });
 });
-// router.post("/test", (req, res) => {
-//     const {
-//         uid,
-//         pass
-//     } = req.body;
-//     user.findOne({
-//         uid
-//     }).then(item => {
-//         console.log(req.headers);
-//         const token = jwt.sign({
-//             name: item.name,
-//             uid: item.uid
-//         }, process.env.SECRET)
-//         res.setHeader("auth", token);
-//         res.send({
-//             message: "Success",
-//             token: token
-//         });
-//     })
-// })
+
 
 module.exports = router;
--- routes/problem.js
+++ routes/problem.js
@@ -3,20 +3,7 @@ const mongoose = require("mongoose");
 const problem = require("../model/problem");
 const report = require("../model/random");
 const auth = require("../config/auth");
-// router.get("/test", auth, (req, res) => {
-//     report.find({}).then(item => {
-//         console.log(req.user)
-//         let problem = [];
-//         item.forEach(values => {
-//             problem.push({
-//                 problem: values.problem
-//             });
-//         })
-//         res.send(
-//             problem
-//         )
-//     }).catch(err => res.send(err))
-// });
+
 router.get("/", auth, (req, res) => {
     report
         .find({
